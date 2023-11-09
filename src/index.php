dfc96a91891223ff022c45467a8861b803685864
kohei23n <kohei.s.n@gmail.com>
2023-11-09 15:18:39 +0900

    update: insertの処理を統一

--- src/index.php
+++ src/index.php
@@ -3,8 +3,9 @@ require 'dbconnect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (isset($_POST['todo-text'])) {
-    $stmt = $dbh->prepare("INSERT INTO todos (text) VALUES (:text)");
-    $stmt->execute([':text' => $_POST['todo-text']]);
+    $stmt = $dbh->prepare('INSERT INTO todos (text) VALUES (:text)');
+    $stmt->bindValue(':text', $_POST['todo-text']);
+    $stmt->execute();
   }
   header('Location: index.php');
   exit;
