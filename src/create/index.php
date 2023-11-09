752aa3f9d4b63bb541c613b7577daaac155c3f59
kohei23n <kohei.s.n@gmail.com>
2023-11-10 00:57:00 +0900

    user_idの処理追加

--- src/create/index.php
+++ src/create/index.php
@@ -8,7 +8,8 @@ if (!isset($_SESSION['id'])) {
 } else {
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['todo-text'])) {
-      $stmt = $dbh->prepare('INSERT INTO todos (text) VALUES (:text)');
+      $stmt = $dbh->prepare('INSERT INTO todos (user_id, text) VALUES (:user_id, :text)');
+      $stmt->bindValue(':user_id', $_SESSION['id']);
       $stmt->bindValue(':text', $_POST['todo-text']);
       $stmt->execute();
     }
