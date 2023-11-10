5c07ff85ce5390b37288644cd87e6bf179ecec1c
kohei23n <kohei.s.n@gmail.com>
2023-11-10 13:44:08 +0900

    update: ヘッダー更新

--- src/components/header.php
+++ src/components/header.php
@@ -1,10 +1,14 @@
 <header>
+  <?php if (isset($_SESSION['email'])) { ?>
   <h1><?= $_SESSION['email']; ?>のTo-Do List</h1>
   <div>
     <form method="POST" action="/auth/logout.php">
       <input type="submit" value="ログアウト" class="submit"/>
     </form>
   </div>
+  <?php } else { ?>
+  <h1>To-Do List</h1>
+  <?php }  ?>
 </header>
 
 <style>
