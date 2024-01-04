dc36ec0c89a3bb1c4c2882ac06529988f5476674
kohei23n <kohei.s.n@gmail.com>
2024-01-04 21:49:39 +0900

    update: headerのstyleをtailwindに更新

--- src/components/header.php
+++ src/components/header.php
@@ -1,4 +1,4 @@
-<header>
+<header class="h-16 bg-cyan-200 flex items-center justify-between px-4 py-6">
   <?php if (isset($_SESSION['email'])) { ?>
   <h1><?= $_SESSION['email']; ?>のTo-Do List</h1>
   <div>
@@ -12,14 +12,6 @@
 </header>
 
 <style>
-header {
-  height: 60px;
-  background-color: #add8e6;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 15px 24px;
-}
 
 .submit {
   cursor: pointer;
