57bb385d4df1be406d1dd73167f7debf68be9289
kohei23n <kohei.s.n@gmail.com>
2024-01-04 21:52:38 +0900

    update: header input のクラスを tailwind に修正

--- src/components/header.php
+++ src/components/header.php
@@ -3,17 +3,10 @@
   <h1><?= $_SESSION['email']; ?>のTo-Do List</h1>
   <div>
     <form method="POST" action="/auth/logout.php">
-      <input type="submit" value="ログアウト" class="submit"/>
+      <input type="submit" value="ログアウト" class="cursor-pointer"/>
     </form>
   </div>
   <?php } else { ?>
   <h1>To-Do List</h1>
-  <?php }  ?>
+  <?php } ?>
 </header>
-
-<style>
-
-.submit {
-  cursor: pointer;
-}
-</style>
